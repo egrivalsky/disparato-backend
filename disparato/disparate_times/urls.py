066cbce8412a981f703b437e6a1d706db363ab1d
unknown <erik@computer.com>
2021-12-29 09:55:13 -0500

    fixes test route

--- disparato/disparate_times/urls.py
+++ disparato/disparate_times/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('test_route/<str:word>', views.test_route, name='test_route'),
     path('related_words/<str:word1>/<str:word2>', views.related_words, name="related_words"),
     path('', views.index, name='index')
+
     
 ]
