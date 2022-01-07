ad709ede63b440f8254df28959e9e2afc6f00425
unknown <erik@computer.com>
2022-01-07 16:00:03 -0500

    Work in progress

--- disparato/disparate_times/urls.py
+++ disparato/disparate_times/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('second_degree_words', views.second_degree_words, name='second_degree_words'),
     path('test_route/<str:word>', views.test_route, name='test_route'),
     path('related_words/<str:word1>/<str:word2>/data.json', views.related_words, name="related_words"),
     path('', views.index, name='index')
--- disparato/disparate_times/views.py
+++ disparato/disparate_times/views.py
@@ -4,10 +4,7 @@ import json
 from datetime import datetime, timedelta
 from datamuse import datamuse
 
-
 datamuse=datamuse.Datamuse()
-
-# Create your views here.
 
 def index(request):
     return HttpResponse('hello')
@@ -23,7 +20,46 @@ def related_words(request, word1, word2):
         related_word = n['word']
         word1_list.append(related_word)
 
-    #now find words related to each word in word1_list
+
+
+    # find words related to word2
+    related2 =  datamuse.words(rel_jja=word2)
+    word2_list = []
+    for n in related2:
+        related_word = n['word']
+        word2_list.append(related_word)
+
+    original_words = {word1, word2}
+    word1_set = set(word1_list)
+    word2_set = set(word2_list)
+    words_in_common = word1_set&word2_set
+    # common_cousins = word1_cuz_set&word2_cuz_set
+
+    # immediate_words = list(words_in_common)
+
+    disparato = {
+        'wordOne': word1,
+        'wordTwo': word2,
+        'immediateWords': list(words_in_common),
+        'wordOneSet': list(word1_set),
+        'wordTwoSet': list(word2_set),
+    }
+
+    
+    # return JsonResponse(disparato)
+    return HttpResponse(json.dumps(disparato), content_type="application/json")
+
+def second_degree_words(request):
+    # lists = request.body
+    disparato = {
+    'wordOne': "hello",
+    'wordTwo': "hi there",
+    }
+    print('SUCCESS*****')
+    return HttpResponse(json.dumps(request.body), content_type="application/json")
+    # print(lists)
+
+    # #find words related to each word in word1_list
     # word1_cousins = []
     # for n in word1_list:
     #     rel_word1 = n
@@ -34,15 +70,9 @@ def related_words(request, word1, word2):
     #         cousin_word = i['word']
     #         word1_cousins.append(cousin_word)
     # word1_cuz_set = set(word1_cousins)
+    # print('word one cousins found')
 
-    # find words related to word2
-    related2 =  datamuse.words(rel_jja=word2)
-    word2_list = []
-    for n in related2:
-        related_word = n['word']
-        word2_list.append(related_word)
-    
-    #now find words related to each word in word2_list
+    # #find words related to each word in word2_list
     # word2_cousins = []
     # for n in word2_list:
     #     rel_word2 = n
@@ -53,29 +83,11 @@ def related_words(request, word1, word2):
     #         cousin_word = i['word']
     #         word2_cousins.append(cousin_word)
     # word2_cuz_set = set(word2_cousins)
+    # print('word one cousins found')
 
-    original_words = {word1, word2}
-    word1_set = set(word1_list)
-    word2_set = set(word2_list)
-    words_in_common = word1_set&word2_set
-    # common_cousins = word1_cuz_set&word2_cuz_set
-
-    immediate_words = list(words_in_common)
-    # cousin_words = list(common_cousins)
-
-    disparato = {
-        'wordOne': word1,
-        'wordTwo': word2,
-        'immediateWords': immediate_words,
-        # 'cousinWords': cousin_words
-    }
-
-    
-    # return JsonResponse(disparato)
-    return HttpResponse(json.dumps(disparato), content_type="application/json")
-
-def second_degree_words():
-    print('second degree words')
+    # second_degree_words = word1_cuz_set&word2_cuz_set
+    # print(list(second_degree_words))
+    # return HttpResponse('second degree words')
 
 def test_route(request, word):
     return HttpResponse('hello from the cloud, and... ' + word)
