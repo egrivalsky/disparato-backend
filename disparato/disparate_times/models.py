c079a1fd56b600bf035dd0dd09da831afc3862bb
unknown <erik@computer.com>
2022-01-27 10:38:54 -0500

    adds database models and migrations

--- disparato/disparate_times/models.py
+++ disparato/disparate_times/models.py
@@ -1,7 +1,16 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
 
 class Word(models.Model):
     word = models.CharField(max_length=100)
-    related_words = models.JSONField(null=True)
+
+class RelatedWord(models.Model):
+    rel_word = models.CharField(max_length=100)
+    rel_score = models.IntegerField()
+
+    word = models.ForeignKey(Word, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return f"{rel_word} has a score of {rel_score}"
