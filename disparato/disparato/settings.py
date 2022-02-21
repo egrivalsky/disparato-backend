ba635b1000c897ffaf6bb4a40a6065c238504eb8
unknown <erik@computer.com>
2022-02-21 15:07:41 -0500

    retrying eb deploy

--- disparato/disparato/settings.py
+++ disparato/disparato/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b7$uuz)c&!sj7#_z(5xo@cj*t*jguv6faljz*^@jo%0$zgl@&x'
+SECRET_KEY = 'b7$uuz)c&!sj7#_z(5xo@cj*t*jguv6faljz*^@jo%0$zgl@&x&!sj7#_z(5xo@cj*t*jguv6faljz*^@jo%0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.1.184']
 
